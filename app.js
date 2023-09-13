5db9a3583077f5d5215597595b685e133d61c9e6
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2023-09-13 08:46:55 +0530

    sets up the express application
    configures product routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+	res.send('<h1>Store API</h1><a href="/api/v1/products">Products Route</a>');
+});
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, console.log(`Server is listening on port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
