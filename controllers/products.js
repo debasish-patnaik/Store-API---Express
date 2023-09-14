1a57f38e2acc81c7a9c9b44057f6a1894518234f
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2023-09-14 20:48:02 +0530

    adds options to filter by featured, company and name fields

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,19 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
+	const { featured, company, name } = req.query;
+
 	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === 'true' ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
 
 	let result = Product.find(queryObject);
 
